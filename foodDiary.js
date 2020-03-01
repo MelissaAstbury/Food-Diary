486d22e9f243f6ec1fc8075583a7bf3b3784b83c
Melissa Astbury <melissajastbury@gmail.com>
2020-03-01 20:27:51 +0000

    JS (This is just for reference - not used to work) Arrays of Objects

---
+++ foodDiary.js
@@ -0,0 +1,71 @@
+// let individualDay = dayLog
+
+let dayLog = [
+    {
+        day: "Monday",
+        breakfast: "Breakfast",
+        lunch: "Lunch",
+        dinner: "Dinner",
+        snacks: "Snacks"
+    },
+    {
+        day: "Tuesday",
+        breakfast: "Breakfast",
+        lunch: "Lunch",
+        dinner: "Dinner",
+        snacks: "Snacks"
+    },
+    {
+        day: "Wednesday",
+        breakfast: "Breakfast",
+        lunch: "Lunch",
+        dinner: "Dinner",
+        snacks: "Snacks"
+    },
+    {
+        day: "Thursday",
+        breakfast: "Breakfast",
+        lunch: "Lunch",
+        dinner: "Dinner",
+        snacks: "Snacks"
+    },
+    {
+        day: "Friday",
+        breakfast: "Breakfast",
+        lunch: "Lunch",
+        dinner: "Dinner",
+        snacks: "Snacks"
+    },
+    {
+        day: "Saturday",
+        breakfast: "Breakfast",
+        lunch: "Lunch",
+        dinner: "Dinner",
+        snacks: "Snacks"
+    },
+    {
+        day: "Sunday",
+        breakfast: "Breakfast",
+        lunch: "Lunch",
+        dinner: "Dinner",
+        snacks: "Snacks"
+    },
+];
+
+// for (i = 0; i < dayLog.length; i++) {
+//     document.writeln(dayLog[6])
+// }
+
+// for (i = 0; i < dayLog.length; i++) {
+//     document.writeln(differentDay[6])
+// }
+
+// dayLog.forEach(element => {
+//     var newDiv = document.createElement("div");
+//     var newContent = document.createTextNode(`${element.day}  ${element.breakfast}  ${element.lunch} ${element.dinner} ${element.snacks}`);
+
+//     newDiv.appendChild(newContent);
+
+//     var currentDiv = document.getElementById("container");
+//     document.body.insertBefore(newDiv, currentDiv);
+// });
